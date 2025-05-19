73778ddfc62eed5e72420323408c9c29b6f54e0c
raphckrman <41128238+raphckrman@users.noreply.github.com>
2025-05-19 15:49:03 +0200

    chore: init

---
+++ src/rest/RESTManager.ts
@@ -0,0 +1,71 @@
+/** @module RESTManager */
+import { RequestOptions } from "../types/request-handler";
+
+export class RestManager {
+  private readonly baseURL: string;
+
+  constructor(baseURL: string) {
+    this.baseURL = baseURL;
+  }
+
+  private async sendRequest<T>(options: RequestOptions): Promise<T> {
+    const { method, path, body, headers } = options;
+    const url = `${this.baseURL}/${path}`;
+
+    const response = await fetch(url, {
+      method,
+      body: body ? JSON.stringify(body) : undefined,
+      headers: {
+        "Content-Type": "application/json",
+        ...headers,
+        "User-Agent": "@raphckrman/turboself-api"
+      }
+    });
+
+    if (!response.ok) {
+      const responseData = await response.json();
+      throw new Error(`${response.status}: ${JSON.stringify(responseData)}`);
+    }
+
+    const responseData = await response.json();
+    return responseData as T;
+  }
+
+  async get<T>(path: string, params?: Record<string, any>, headers?: Record<string, string>): Promise<T> {
+    const urlParams = new URLSearchParams(params).toString();
+    const urlPath = urlParams ? `${path}?${urlParams}` : path;
+    return this.sendRequest<T>({
+      method: "GET",
+      path: urlPath,
+      headers: headers
+    });
+  }
+
+  async post<T>(path: string, body: any, options?: RequestOptions): Promise<T> {
+    return this.sendRequest<T>({
+      method: "POST",
+      path,
+      body,
+      headers: options?.headers
+    });
+  }
+
+  async put<T>(path: string, body: any, options?: RequestOptions): Promise<T> {
+    return this.sendRequest<T>({
+      method: "PUT",
+      path,
+      body,
+      headers: options?.headers
+    });
+  }
+
+  async delete<T>(path: string, params?: Record<string, any>, options?: RequestOptions): Promise<T> {
+    const urlParams = new URLSearchParams(params).toString();
+    const urlPath = urlParams ? `${path}?${urlParams}` : path;
+    return this.sendRequest<T>({
+      method: "DELETE",
+      path: urlPath,
+      headers: options?.headers
+    });
+  }
+}
---
+++ src/rest/endpoints.ts
@@ -0,0 +1,4 @@
+/** REST/Endpoints */
+export const BASE_URL = "https://api.skolengo.com/api/v1/bff-sko-app";
+
+export const SEARCH_SCHOOLS = "/schools"
